381527f177187e0c4cf2b380e4f0a95e435540f1
Valache Eduard <valacheeduard@gmail.com>
2014-07-10 19:54:12 +0300

    config.php

---
+++ classes/SNDTU_Questions.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: ASSus
+ * Date: 7/5/14
+ * Time: 4:12 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class SNDTU_Questions {
+
+}
